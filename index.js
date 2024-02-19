484c5972dbc67b14536e380a5b7142916f59eef0
francis-gomes-esq <figomesokoye@protonmail.com>
2024-02-19 01:39:04 +0000

    add usage instruction and license options

--- index.js
+++ index.js
@@ -16,19 +16,37 @@ function init() {
 			{
 				type: 'input',
 				name: 'title',
-				message: 'what is your-project-title?',
+				message: 'What is the title of your project?',
 			},
 			{
 				type: 'input',
 				name: 'description',
-				message:
-					'Provide a short description explaining of your project.',
+				message: 'Provide a short description explaining your project.',
 			},
 
 			{
 				type: 'input',
 				name: 'installation',
 				message: 'What are the steps required to install your project?',
+			},
+
+			{
+				type: 'input',
+				name: 'usage',
+				message: 'Provide instructions and examples for use:',
+			},
+			{
+				type: 'list',
+				name: 'license',
+				message: 'Choose a license for your project:',
+				choices: [
+					'Apache License 2.0',
+					'MIT License',
+					'GNU General Public License v3.0',
+					'Creative Commons Zero v.10 Universal',
+					'Mozilla Public License 2.0',
+					'The Unlicense',
+				],
 			},
 		])
 		.then(answers => {
