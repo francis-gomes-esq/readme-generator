076b3bc7c63ea4169aefd1ac98ce67f461300358
francis-gomes-esq <figomesokoye@protonmail.com>
2024-02-19 01:25:54 +0000

    add table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,12 +4,19 @@ function generateMarkdown(data) {
 	return `
 	# ${data.title}
 	
-	## Description
-	
-	${data.description}	
+	## Description	
+	${data.description}
+
+	## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Email](#email)
+  - [GitHub](#github)
 	
 	## Installation
-
 	${data.installation}
 
 `;
